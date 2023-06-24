fc0a1c4ab5ef4662f6590a8bce7045cf4d5bbd8d
SlipStreme <Kinloes@gmail.com>
2023-06-24 18:03:18 -0400

    Finished the program! It will now continue playing until 5 rounds have passed Once 5 rounds pass the program provides the results After providing results the program ends

--- rps.js
+++ rps.js
@@ -1,10 +1,13 @@
-const rps = ["rock", "paper", "scissors"];
-let answer;
+const rps = ["rock", "paper", "scissors"]
+let answer
+let wins = 0
+let losses = 0
+let counter = 1
 game()
 function game(){
 function getComputerChoice(){
     answer = (rps[(Math.floor(Math.random() * rps.length))])}
-let choice = prompt(`Lets play a game. Type "rock, paper, or scissors" `).toLowerCase();
+let choice = prompt(`Lets play a game. Type "rock, paper, or scissors" `).toLowerCase()
 getComputerChoice()
 console.log (answer)
 
@@ -14,12 +17,17 @@ if (choice == "rock"){
 }
 if (choice == "rock" && answer ==  "rock"){
     alert ("I also chose rock. It's a tie!")
+    newRound()
 }
 if (choice == "rock" && answer == "scissors"){
     alert ("Your rock smashes my scissors! You win!")
+    wins += 1
+    newRound()
 }
 if (choice == "rock" && answer == "paper"){
     alert ("My paper covers your rock! You lose!")
+    losses += 1
+    newRound()
 }
 
 // if player chooses paper
@@ -28,12 +36,17 @@ if (choice == "paper") {
 }
 if (choice == "paper" && answer == "paper"){
     alert ("I also chose paper. It's a tie!")
+    newRound()
 }
 if (choice == "paper" && answer == "scissors"){
     alert ("I cut your paper into pieces with my scissors! You lose!")
+    losses += 1
+    newRound()
 }
 if (choice == "paper" && answer == "rock"){
     alert ("Your paper wraps up my rock! You win!")
+    wins += 1
+    newRound()
 }
 
 // if player chooses scissors
@@ -42,12 +55,17 @@ if (choice == "scissors"){
 }
 if (choice == "scissors" && answer == "scissors"){
     alert ("I also chose scissors. It's a tie!")
+    newRound()
 }
 if (choice == "scissors" && answer == "rock"){
     alert ("My rock smashes your scissors! You lose!")
+    losses += 1
+    newRound()
 }
 if (choice == "scissors" && answer == "paper"){
     alert ("You cut my paper into pieces! You win!")
+    wins += 1
+    newRound()
 }
 
 //if player choice is invalid
@@ -55,3 +73,45 @@ else if (choice != "rock" && choice != "paper" && choice != "scissors"){
     alert (choice + " does not compute. Please choose rock, paper, or scissors")
 }
 }
+
+//plays a new round or ends the game after 5 games
+newRound()
+function newRound(){
+alert (`You have ${wins} wins and ${losses} losses`)
+if (counter == 5){
+    alert("That's a wrap!")
+    if (wins >= losses){
+        alert("Congrats! You win!")
+        endGame()
+    }
+    if (wins == losses){
+        alert("Looks like we tied! That's boring.")
+        endGame()
+    }
+    if (wins <= losses){
+        alert("Sorry, you lose!")
+        endGame()
+    }
+}
+//after a round is complete
+let YN = prompt (`We've played ${counter} games. Would you like to play again? Input yes or no.`).toLowerCase()
+if (YN == "yes"){
+    counter += 1
+    game()
+}
+if (YN == "no"){
+    alert ("Goodbye")
+    endGame()
+}
+if (YN != "yes" && YN != "no"){
+    alert ("Please input yes or no")
+    newRound()
+}
+}
+//This function ends the script
+function endGame(){
+    throw error()
+}
+console.log (wins)
+console.log (losses)
+console.log (counter)
