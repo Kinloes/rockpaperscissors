9ba650ee1ad987d121f0141204809285cc3f9e01
SlipStreme <Kinloes@gmail.com>
2023-07-17 17:45:57 -0400

    Added the finishing touches Removed page content after 5 rounds Added text that displays when the game is over Replaced alerts with text that updates and explains the result of the round

--- rps.js
+++ rps.js
@@ -1,8 +1,10 @@
-const rps = ["rock", "paper", "scissors"]
-let answer
-let wins = 0
-let losses = 0
-let counter = 0
+const rps = ["rock", "paper", "scissors"];
+let answer;
+let wins = 0;
+let losses = 0;
+let counter = 0;
+let reply = document.getElementById("reply");
+let para = document.getElementById("results");
 const rock = document.getElementById("r1");
 const paper = document.getElementById("p1");
 const scissors = document.getElementById("s1");
@@ -17,22 +19,21 @@ console.log (answer);
 
 // if player chooses rock 
 function rockTrue(){
-    alert ("You chose rock!");
 if (answer ==  "rock"){
-    alert ("I also chose rock. It's a tie!");
+    reply.textContent = "I also chose rock - It's a tie!";
     counter += 1;
     newRound();
     return;
 }
 else if (answer == "scissors"){
-    alert ("Your rock smashes my scissors! You win!");
+    reply.textContent = "Your rock smashes my scissors!";
     wins += 1;
     counter += 1;
     newRound();
     return;
 }
 else if (answer == "paper"){
-    alert ("My paper covers your rock! You lose!");
+    reply.textContent = "My paper covers up your rock!";
     losses += 1;
     counter += 1;
     newRound();
@@ -40,41 +41,38 @@ else if (answer == "paper"){
 }
 }
 function paperTrue(){
-    alert ("You chose paper");
     if (answer == "paper"){
-        alert ("I also chose paper. It's a tie!");
+        reply.textContent = "I also chose paper - It's a tie";
         counter += 1;
         newRound();
     }
     else if (answer == "scissors"){
-        alert ("I cut your paper into pieces with my scissors! You lose!");
+        reply.textContent = "My scissors cut up your paper!";
         losses += 1;
         counter += 1;
         newRound();
     }
     else if (answer == "rock"){
-        alert ("Your paper wraps up my rock! You win!");
+        reply.textContent = "Your paper wraps up my rock!";
         wins += 1;
         counter += 1;
         newRound();
     }
 }
 function scissorsTrue(){
-    alert ("You chose scissors");
-
-if (answer == "scissors"){
-    alert ("I also chose scissors. It's a tie!");
+    if (answer == "scissors"){
+    reply.textContent = "I also chose scissors - It's a tie";
     counter += 1;
     newRound();
 }
-else if (answer == "rock"){
-    alert ("My rock smashes your scissors! You lose!");
+    else if (answer == "rock"){
+    reply.textContent = "My rock smashes your scissors!";
     losses += 1;
     counter += 1;
     newRound();
 }
-else if (answer == "paper"){
-    alert ("You cut my paper into pieces! You win!");
+    else if (answer == "paper"){
+    reply.textContent = "You cut up my paper with your scissors!";
     wins += 1;
     counter += 1;
     newRound();
@@ -83,9 +81,8 @@ else if (answer == "paper"){
 
 //plays a new round or ends the game after 5 rounds
 function newRound(){
-    getComputerChoice()
-    console.log (answer)
-    let para = document.getElementById("results");
+    getComputerChoice();
+    console.log (answer);
     if (counter == 1){
         para.textContent = `You have ${wins} wins and ${losses} losses`;
         document.body.appendChild(para);
@@ -94,25 +91,36 @@ function newRound(){
     para.textContent = ``;
     document.body.appendChild(para);
     para.textContent = `You have ${wins} wins and ${losses} losses`;
-    document.body.appendChild(para)
+    document.body.appendChild(para);
     }
-/*if (counter === 5){
-    alert("That's a wrap!");
-    if (wins >= losses){
-        alert("Congrats! You win!");
+if (counter === 5){
+    let final = document.getElementById("gameOver");
+    final.textContent = "That's 5! Game Over!"
+    if (wins > losses){
+        let finalResults = document.getElementById("finalResults");
+        finalResults.textContent = "Congrats! You Win!"
         endGame();}
 
-    if (wins == losses){
-        alert("Looks like we tied! That's boring.");
+    if (wins === losses){
+        let finalResults = document.getElementById("finalResults");
+        finalResults.textContent = "Looks like we tied... That's boring!"
         endGame();}
 
-    if (wins <= losses){
-        alert("Sorry, you lose!");
+    if (wins < losses){
+        let finalResults = document.getElementById("finalResults");
+        finalResults.textContent = "Nice try, but you lost this time!"
         endGame();}
-}*/
+}
 }
 
 //This function ends the script
 function endGame(){
-    throw error()
+    let del = document.getElementById("content");
+    del.remove();
+    para.textContent = `You won ${wins} games and I won ${losses} games`;
+    document.body.appendChild(para);
+    let replay = document.getElementById("playAgain");
+    document.body.appendChild(replay);
+    replay.textContent = "Refresh the page to play again!";
+    
 }
