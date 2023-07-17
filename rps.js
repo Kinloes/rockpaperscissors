5f9bae63b1a09974dd744f31b277c58468b4f325
SlipStreme <Kinloes@gmail.com>
2023-07-16 20:34:56 -0400

    Fixed a looping error Error resulted from nesting "if" statements Game now runs as it should.

--- rps.js
+++ rps.js
@@ -2,116 +2,111 @@ const rps = ["rock", "paper", "scissors"]
 let answer
 let wins = 0
 let losses = 0
-let counter = 1
-game()
-function game(){
+let counter = 0
+const rock = document.getElementById("r1");
+const paper = document.getElementById("p1");
+const scissors = document.getElementById("s1");
+rock.addEventListener("click", rockTrue);
+paper.addEventListener("click", paperTrue);
+scissors.addEventListener("click", scissorsTrue);
 function getComputerChoice(){
     answer = (rps[(Math.floor(Math.random() * rps.length))])}
-// <OBSOLETE> let choice = prompt(`Lets play a game. Type "rock, paper, or scissors" `).toLowerCase()
-getComputerChoice()
-console.log (answer)
+
+getComputerChoice();
+console.log (answer);
 
 // if player chooses rock 
-if (choice == "rock"){
-    alert ("You chose rock")
+function rockTrue(){
+    alert ("You chose rock!");
+if (answer ==  "rock"){
+    alert ("I also chose rock. It's a tie!");
+    counter += 1;
+    newRound();
+    return;
 }
-if (choice == "rock" && answer ==  "rock"){
-    alert ("I also chose rock. It's a tie!")
-    newRound()
+else if (answer == "scissors"){
+    alert ("Your rock smashes my scissors! You win!");
+    wins += 1;
+    counter += 1;
+    newRound();
+    return;
 }
-if (choice == "rock" && answer == "scissors"){
-    alert ("Your rock smashes my scissors! You win!")
-    wins += 1
-    newRound()
+else if (answer == "paper"){
+    alert ("My paper covers your rock! You lose!");
+    losses += 1;
+    counter += 1;
+    newRound();
+    return;
 }
-if (choice == "rock" && answer == "paper"){
-    alert ("My paper covers your rock! You lose!")
-    losses += 1
-    newRound()
+}
+function paperTrue(){
+    alert ("You chose paper");
+    if (answer == "paper"){
+        alert ("I also chose paper. It's a tie!");
+        counter += 1;
+        newRound();
+    }
+    else if (answer == "scissors"){
+        alert ("I cut your paper into pieces with my scissors! You lose!");
+        losses += 1;
+        counter += 1;
+        newRound();
+    }
+    else if (answer == "rock"){
+        alert ("Your paper wraps up my rock! You win!");
+        wins += 1;
+        counter += 1;
+        newRound();
+    }
+}
+function scissorsTrue(){
+    alert ("You chose scissors");
+
+if (answer == "scissors"){
+    alert ("I also chose scissors. It's a tie!");
+    counter += 1;
+    newRound();
+}
+else if (answer == "rock"){
+    alert ("My rock smashes your scissors! You lose!");
+    losses += 1;
+    counter += 1;
+    newRound();
+}
+else if (answer == "paper"){
+    alert ("You cut my paper into pieces! You win!");
+    wins += 1;
+    counter += 1;
+    newRound();
+}
 }
 
-// if player chooses paper
-if (choice == "paper") {
-    alert ("You chose paper")
-}
-if (choice == "paper" && answer == "paper"){
-    alert ("I also chose paper. It's a tie!")
-    newRound()
-}
-if (choice == "paper" && answer == "scissors"){
-    alert ("I cut your paper into pieces with my scissors! You lose!")
-    losses += 1
-    newRound()
-}
-if (choice == "paper" && answer == "rock"){
-    alert ("Your paper wraps up my rock! You win!")
-    wins += 1
-    newRound()
-}
-
-// if player chooses scissors
-if (choice == "scissors"){
-    alert ("You chose scissors")
-}
-if (choice == "scissors" && answer == "scissors"){
-    alert ("I also chose scissors. It's a tie!")
-    newRound()
-}
-if (choice == "scissors" && answer == "rock"){
-    alert ("My rock smashes your scissors! You lose!")
-    losses += 1
-    newRound()
-}
-if (choice == "scissors" && answer == "paper"){
-    alert ("You cut my paper into pieces! You win!")
-    wins += 1
-    newRound()
-}
-
-//if player choice is invalid
-else if (choice != "rock" && choice != "paper" && choice != "scissors"){
-    alert (choice + " does not compute. Please choose rock, paper, or scissors")
-}
-}
-
-//plays a new round or ends the game after 5 games
-newRound()
+//plays a new round or ends the game after 5 rounds
 function newRound(){
-alert (`You have ${wins} wins and ${losses} losses`)
-if (counter == 5){
-    alert("That's a wrap!")
+    getComputerChoice()
+    console.log (answer)
+    const para = document.createElement('p');
+    //const del = document.getElementById('p');
+    //para.remove();
+    para.textContent = `You have ${wins} wins and ${losses} losses`;
+    document.body.appendChild(para);
+    para.style.color = "blue";
+if (counter === 5){
+    alert("That's a wrap!");
     if (wins >= losses){
-        alert("Congrats! You win!")
-        endGame()
-    }
+        alert("Congrats! You win!");
+        endGame();}
+
     if (wins == losses){
-        alert("Looks like we tied! That's boring.")
-        endGame()
-    }
+        alert("Looks like we tied! That's boring.");
+        endGame();}
+
     if (wins <= losses){
-        alert("Sorry, you lose!")
-        endGame()
-    }
-}
-//after a round is complete
-let YN = prompt (`We've played ${counter} games. Would you like to play again? Input yes or no.`).toLowerCase()
-if (YN == "yes"){
-    counter += 1
-    game()
-}
-if (YN == "no"){
-    alert ("Goodbye")
-    endGame()
-}
-if (YN != "yes" && YN != "no"){
-    alert ("Please input yes or no")
-    newRound()
-}
-}
+        alert("Sorry, you lose!");
+        endGame();}
+}}
+
 //This function ends the script
 function endGame(){
     throw error()
 }
-console.log (wins)
-console.log (losses)
-console.log (counter)
